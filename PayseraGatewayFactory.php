5135e2de23e3486431d4e7e5390ec8488f86dd94
Mykolas <mykolasvitkus@gmail.com>
2019-08-13 14:31:13 +0300

    naming, .gitignore

--- PayseraGatewayFactory.php
+++ PayseraGatewayFactory.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace PlumTreeSystems\Paysera;
 
 use PlumTreeSystems\Paysera\Action\AuthorizeAction;
@@ -11,7 +12,7 @@ use PlumTreeSystems\Paysera\Action\StatusAction;
 use Payum\Core\Bridge\Spl\ArrayObject;
 use Payum\Core\GatewayFactory;
 
-class SkeletonGatewayFactory extends GatewayFactory
+class PayseraGatewayFactory extends GatewayFactory
 {
     /**
      * {@inheritDoc}
@@ -32,15 +33,19 @@ class SkeletonGatewayFactory extends GatewayFactory
 
         if (false == $config['payum.api']) {
             $config['payum.default_options'] = array(
+                'project_id' => '',
+                'password' => '',
                 'sandbox' => true,
             );
             $config->defaults($config['payum.default_options']);
-            $config['payum.required_options'] = [];
+            $config['payum.required_options'] = [
+                'project_id', 'password'
+            ];
 
             $config['payum.api'] = function (ArrayObject $config) {
                 $config->validateNotEmpty($config['payum.required_options']);
 
-                return new Api((array) $config, $config['payum.http_client'], $config['httplug.message_factory']);
+                return new Api((array)$config, $config['payum.http_client'], $config['httplug.message_factory']);
             };
         }
     }
